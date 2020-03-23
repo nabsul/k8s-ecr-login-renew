84c53ea23b349d92a44aa4b6b1a71e39a1e245f1
Nabeel Sulieman <nabsul@outlook.com>
2020-03-22 17:02:22 -0700

    Rename license and readme. Add latest tag to push script.

--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
 docker build -t nabsul/k8s-ecr-login-renew .
 docker tag nabsul/k8s-ecr-login-renew:latest nabsul/k8s-ecr-login-renew:$1
+docker tag nabsul/k8s-ecr-login-renew:latest nabsul/k8s-ecr-login-renew:latest
 docker push nabsul/k8s-ecr-login-renew:$1
+docker push nabsul/k8s-ecr-login-renew:latest
