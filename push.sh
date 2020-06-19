5205f48fb4a5fa810fcc4eb75fe874d3da0b7037
Nabeel Sulieman <nabsul@outlook.com>
2020-06-19 16:26:10 -0700

    Update push.sh to do x86 and ARM

--- push.sh
+++ push.sh
@@ -1,5 +1,4 @@
-docker build -t nabsul/k8s-ecr-login-renew .
-docker tag nabsul/k8s-ecr-login-renew:latest nabsul/k8s-ecr-login-renew:$1
-docker tag nabsul/k8s-ecr-login-renew:latest nabsul/k8s-ecr-login-renew:latest
+docker build -t nabsul/k8s-ecr-login-renew:$1 .
+docker build -t nabsul/k8s-ecr-login-renew:arm32v7-$1 -f Dockerfile-arm .
 docker push nabsul/k8s-ecr-login-renew:$1
-docker push nabsul/k8s-ecr-login-renew:latest
+docker push nabsul/k8s-ecr-login-renew:arm32v7-$1
