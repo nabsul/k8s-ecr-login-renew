ac3fe390b4dc1415ec8570f0bc16de154eb33ef6
Ashutosh Angiras <aangiras@de-c-304.cinemo-de.internal>
2021-01-20 10:30:16 +0100

    update vault

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
-
-	// "github.com/nabsul/k8s-ecr-login-renew/src/aws"
-	// "github.com/nabsul/k8s-ecr-login-renew/src/k8s"
-	"github.com/nabsul/k8s-ecr-login-renew/src/aws"
-	"github.com/nabsul/k8s-ecr-login-renew/src/vault"
-
-	// "os"
 	"time"
+
+	"github.com/nabsul/k8s-ecr-login-renew/src/aws"
+	"github.com/nabsul/k8s-ecr-login-renew/src/k8s"
+	"github.com/nabsul/k8s-ecr-login-renew/src/vault"
 )
 
 const (
@@ -76,25 +74,25 @@ func main() {
 		fmt.Println("Success.")
 	}
 
-	// namespaces, err := k8s.GetNamespaces(namespaceList)
-	// checkErr(err)
-	// fmt.Printf("Updating kubernetes secret [%s] in %d namespaces\n", name, len(namespaces))
+	namespaces, err := k8s.GetNamespaces(namespaceList)
+	checkErr(err)
+	fmt.Printf("Updating kubernetes secret [%s] in %d namespaces\n", name, len(namespaces))
 
-	// failed := false
-	// for _, ns := range namespaces {
-	// 	fmt.Printf("Updating secret in namespace [%s]... ", ns)
-	// 	err = k8s.UpdatePassword(ns, name, username, password, server)
-	// 	if nil != err {
-	// 		fmt.Printf("failed: %s\n", err)
-	// 		failed = true
-	// 	} else {
-	// 		fmt.Println("success")
-	// 	}
-	// }
+	failed := false
+	for _, ns := range namespaces {
+		fmt.Printf("Updating secret in namespace [%s]... ", ns)
+		err = k8s.UpdatePassword(ns, name, username, password, server)
+		if nil != err {
+			fmt.Printf("failed: %s\n", err)
+			failed = true
+		} else {
+			fmt.Println("success")
+		}
+	}
 
-	// if failed {
-	// 	panic(errors.New("failed to create one of more Docker login secrets"))
-	// }
+	if failed {
+		panic(errors.New("failed to create one of more Docker login secrets"))
+	}
 
 	fmt.Println("Job complete.")
 }
